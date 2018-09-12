ca8ab5e626b2b64483f55ad0bf7dced596c2beb7
Kevin McAleer <kevinmcaleer@gmail.com>
2018-09-12 09:21:28 +0100

    Added new class

--- SMARS_Python.py
+++ SMARS_Python.py
@@ -1,2 +1,7 @@
-''' SMARS Python library
-''' Kevin McAleer September 2018
+' SMARS Python library
+' Kevin McAleer September 2018
+' Purpose: Provides library routines for the SMARS robot and quad robots
+
+class SmarsRobot():
+    type = ['wheel', 'quad'] # defines if the robot is a quad or wheel based robot
+    
