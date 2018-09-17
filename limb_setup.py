6d2eacc51b3a55419c2589ddcc8844f5a8a8a888
Kevin McAleer <kevinmcaleer@gmail.com>
2018-09-17 23:04:08 +0100

    limb setup

--- limb_setup.py
+++ limb_setup.py
@@ -9,12 +9,12 @@ l1.name = "left_leg_front"
 l1.channel = 0
 
 # print "setting minimum position"
-position = 0
+position = 300
 key = ""
 while key != "0":
     key = raw_input("setting min position, press 0 to exit")
     print key
-    # if raw_input == 
+    # if raw_input ==
     position = position + 10
     l1.moveTo(position)
     print "current position:", position
