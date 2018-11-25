bde1d243fb0b48a1f72105e0d8baf721d1ade65b
Kevin McAleer <kevinmcaleer@gmail.com>
2018-11-25 14:33:09 +0000

    Added delay to clap

--- SMARS_Library.py
+++ SMARS_Library.py
@@ -436,7 +436,8 @@ class SmarsRobot(object):
         for n in range (0, clap_count):
             self.legs[left_leg_front].setBody()
             self.legs[right_leg_front].setBody()
-            time.sleep(sleep_count)
+            time.sleep(sleep_count * 10)
             self.legs[left_leg_front].setStretch()
             self.legs[right_leg_front].setStretch()
-            time.sleep(sleep_count)
+            time.sleep(sleep_count * 10)
+        self.stand()
