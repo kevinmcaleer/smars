14df4c9b3c7c58759212b39dc888794ec461f3b5
Kevin McAleer <kevinmcaleer@gmail.com>
2018-11-25 16:09:37 +0000

    updated SCode Parser

--- S-CodeParse.py
+++ S-CodeParse.py
@@ -7,11 +7,11 @@ from SMARS_Library import SmarsRobot
 from SMARS_Library import leg
 import time
 
-sm = SmarsRobot()
-sm.setName("Quaddy")
-sm.type = "Quad"
+def readLine(line):
+    sm = SmarsRobot()
+    sm.setName("Quaddy")
+    sm.type = "Quad"
 
-def readLine(self, line):
     if line[1] == 's101': # Move Forward
         sm.walkForward(100)
     if line[1] == 's102': # Move Backward
@@ -20,4 +20,7 @@ def readLine(self, line):
         sm.turnLeft()
     if line[1] == 's104': # Turn right
         sm.turnRight()
-    
+
+def __main__():
+    key = raw_input("# ")
+    readLine(key)
