b9a1dddbf0868e7711f335a3aa4fcbc87dec45f2
Kevin McAleer <kevinmcaleer@me.com>
2018-12-02 21:50:08 +0000

    Added setAngle to imports

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,7 @@
 import unittest
 from SMARS_Library import leg
 from SMARS_Library import set_servo_pulse
+from SMARS_Library import setAngle 
 
 class setServoPulseTestCase(unittest.TestCase):
     """ tests setServoPulse """
