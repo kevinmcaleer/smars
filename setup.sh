fde4d897bd7548c7c25d2e13c679bd040b6c6f92
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 19:43:58 +0000

    added github clone

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Setting up SMARS Python Environment
 echo -----------------------------------
+git clone https://www.github.com/kevincaleer/smars 
 pip install virtualenv
 virtualenv venv
 cd venv
