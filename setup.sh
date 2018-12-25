b9204f2dc190c025cde07f7eb10367978a76e120
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 19:08:42 +0000

    Setup includes pipenv

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 echo Setting up SMARS Python Environment
 echo -----------------------------------
 echo
+sudo pip install pipenv
+sudo pipenv install
 sudo apt-get install python-smbus
 sudo apt-get install i2c-tools
