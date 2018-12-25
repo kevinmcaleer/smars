010120c1c5eeb21a3a58767ca14cff3c910a6919
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 19:17:47 +0000

    removed echo. from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo Setting up SMARS Python Environment
 echo -----------------------------------
-echo.
 sudo pip install pipenv
 sudo pipenv install
 sudo apt-get install python-smbus
