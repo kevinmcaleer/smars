5e9b5a86761a84977c8821b017049dab3ce89925
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 20:25:18 +0000

    enable i2c-dev interface

--- setup.sh
+++ setup.sh
@@ -8,6 +8,13 @@ virtualenv venv
 cd venv
 source venv/bin/activate
 pip install adafruit-pca9685
+pip install smbus2
 # sudo pipenv install
-sudo apt-get install python-smbus
+# sudo apt-get install python-smbus
 sudo apt-get install i2c-tools
+echo enabling i2c Interface 
+if grep -q 'i2c-dev' /etc/modules; then
+  echo 'Seems i2c-dev module already exists, skip this step.'
+else
+  echo 'i2c-dev' >> /etc/modules
+fi
