f6ee7894dfe83fb7cfeff9e7abd09973abd0321d
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 20:11:34 +0000

    updated setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo Setting up SMARS Python Environment
 echo -----------------------------------
-git clone https://github.com/kevinmcaleer/smars.git 
+git clone https://github.com/kevinmcaleer/smars.git
 pip install virtualenv
+cd smars 
 virtualenv venv
 cd venv
 pip install adafruit-pca9685
