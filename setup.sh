7351a08b430ecbdbb15fb17cbd3938e56a6e77e8
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 19:42:20 +0000

    added adafruit pca9685 driver library

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 echo Setting up SMARS Python Environment
 echo -----------------------------------
 pip install virtualenv
+virtualenv venv
+cd venv
+pip install adafruit-pca9685
 # sudo pipenv install
 sudo apt-get install python-smbus
 sudo apt-get install i2c-tools
