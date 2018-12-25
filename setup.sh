9ddcf05dd23a8c450bc331f51c52d72c3e0ecd63
Kevin McAleer <kevinmcaleer@me.com>
2018-12-25 19:34:53 +0000

    remvoed pipenv from setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 echo Setting up SMARS Python Environment
 echo -----------------------------------
 pip install virtualenv
-sudo pipenv install
+# sudo pipenv install
 sudo apt-get install python-smbus
 sudo apt-get install i2c-tools
