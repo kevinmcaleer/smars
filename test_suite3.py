146a3b511f2f72651981ba1adc2f02bc3202305d
Kevin McAleer <kevinmcaleer@me.com>
2019-03-12 12:27:43 +0000

    test

--- test_suite3.py
+++ test_suite3.py
@@ -17,13 +17,6 @@ class SetServoPulseTestCase(unittest.TestCase):
         for channel in range(0, 16):
             self.assertTrue(set_servo_pulse(channel, 10))
 
-    # def test_setServoPulseIsNumberBetween0And4096(self):
-    #     self.assertTrue(set_servo_pulse(0, 0))
-    #     self.assertTrue(set_servo_pulse(0, 4096))
-    #     self.assertTrue(set_servo_pulse(0, 2000))
-    #     self.assertFalse(set_servo_pulse(0, 4097))
-    #     self.assertFalse(set_servo_pulse(0, -1))
-
     def testsetservopulseisnumnerbetween0and4096(self):  # Python 3 library test
         self.assertTrue(set_servo_pulse(0, 0))
         self.assertTrue(set_servo_pulse(0, 4096))
@@ -31,18 +24,11 @@ class SetServoPulseTestCase(unittest.TestCase):
         self.assertFalse(set_servo_pulse(0, 4097))
         self.assertFalse(set_servo_pulse(0, -1))
 
-    # def test_setangle(self):
-    #     legtest = leg()  # type: leg
-    #     self.assertTrue(legtest.setAngle(0))
-    #     self.assertTrue(legtest.setAngle(180))
-    #     self.assertFalse(legtest.setAngle(181))
-
     def test_setangle3(self):  # Python 3 library test
         legtest3 = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")  # type: Leg
         self.assertTrue(legtest3.setangle(0))
         self.assertTrue(legtest3.setangle(180))
         self.assertFalse(legtest3.setangle(181))
-
 
 class TestLegSetdefault(unittest.TestCase):
     """ tests leg setdefault() """
