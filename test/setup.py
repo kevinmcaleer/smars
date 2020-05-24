95231548b565b5b0babaf139056b5dafb49a0fc7
Kevin McAleer <kevinmcaleer@me.com>
2020-05-24 15:19:08 +0100

    Updated travis.yml and added setup.py

---
+++ test/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='smars',
+      version='0.1.12',
+      description='SMARS Quad & Wheeled test harness',
+      author='Kevin McAleer',
+      author_email='kevinmcaleer@gmail.com',
+      url='www.smarsfan.com',
+      packages=find_packages(),
+     )
